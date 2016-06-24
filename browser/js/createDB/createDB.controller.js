003bead211d29c27730eb6abae09055959ca43d1
Alfonso Antonio <j.alfonso.antonio@gmail.com>
2016-06-24 12:05:43 -0400

    Update createDB.controller.js

--- browser/js/createDB/createDB.controller.js
+++ browser/js/createDB/createDB.controller.js
@@ -1,15 +1,7 @@
 app.controller('CreatedbCtrl', function ($scope, CreatedbFactory) {
 
 	$scope.createdDB = false;
-
-	// $scope.columnIndex = 0;
-
-	// $scope.increment = function() {
-	// 	$scope.columnIndex ++;
-	// 	$scope.$digest();
-	// }
-
-	$scope.columnArray = [];
+        $scope.columnArray = [];
 
 	$scope.add = function() {
 		$scope.columnArray.push('1');
@@ -19,7 +11,6 @@ app.controller('CreatedbCtrl', function ($scope, CreatedbFactory) {
 		CreatedbFactory.createDB(name)
 		.then(function(data) {
 			$scope.createdDB = data;
-			console.log(data);
 		})
 	}
 
