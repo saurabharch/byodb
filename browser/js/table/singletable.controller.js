eac45fcdc807d455c18a69d10b6272e982d23d35
matthewlanders <matthewlanders10@gmail.com>
2016-06-26 23:00:09 -0400

    can edit content table and save the changed on front end

--- browser/js/table/singletable.controller.js
+++ browser/js/table/singletable.controller.js
@@ -21,4 +21,35 @@ app.controller('SingleTableCtrl', function ($scope, singleTable) {
 		$scope.instanceArray.push(rowValues)
 	})
 
+	
+	$scope.valsToUpdate = [];
+
+	$scope.update = function(old, val, row, i){
+		row[i] = val;
+		var rowObj = {};
+		var cols = $scope.columns;
+		for(var c = 0; c < cols.length; c++){
+			var colName = cols[c];
+			rowObj[colName] = row[c];
+		}
+
+		console.log(val)
+
+		// if there is nothing in the array to update, push the update into it
+		if($scope.valsToUpdate.length === 0) $scope.valsToUpdate.push(rowObj);
+
+		// check to see if the row is already scheduled to be updated, if it is, then update it with the new thing to be updated
+		for(var e = 0; e < $scope.valsToUpdate.length; e++){
+			if($scope.valsToUpdate[e].id === rowObj.id) $scope.valsToUpdate[e] = rowObj;
+			else $scope.valsToUpdate.push(rowObj);
+		}
+
+	}
+
+	$scope.logData=function () {
+		console.log($scope.valsToUpdate)
+        // if(employee !== undefined) $scope.editEmployee.push(employee);
+        // console.log($scope.editEmployee);
+     }
+
 });
