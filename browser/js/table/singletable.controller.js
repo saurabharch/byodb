daf14deb398e3d4aa2d147d7f1728eb3e89e71b6
matthewlanders <matthewlanders10@gmail.com>
2016-06-26 23:37:34 -0400

    can safely update column values and row values

--- browser/js/table/singletable.controller.js
+++ browser/js/table/singletable.controller.js
@@ -3,11 +3,13 @@ app.controller('SingleTableCtrl', function ($scope, singleTable) {
 
 	// Get all of the columns to create the columns on the bootstrap table
 	$scope.columns = [];
+	$scope.originalColVals = [];
 
 	var table = singleTable[0];
 
 	for(var prop in table){
-		if(prop !== 'created_at' && prop !== 'updated_at') $scope.columns.push(prop)
+		if(prop !== 'created_at' && prop !== 'updated_at') $scope.columns.push(prop);
+		if(prop !== 'created_at' && prop !== 'updated_at') $scope.originalColVals.push(prop)
 	}
 
 	// Sort the values in sigleTable so that all the values for a given row are grouped
@@ -21,33 +23,57 @@ app.controller('SingleTableCtrl', function ($scope, singleTable) {
 		$scope.instanceArray.push(rowValues)
 	})
 
-	
-	$scope.valsToUpdate = [];
 
-	$scope.update = function(old, val, row, i){
+	//************ Important *********
+		// Make sure to update the row values BEFORE the column name
+			// The rowValsToUpdate array stores the values of the ORIGINAL column names so if the column name is updated after the row value, we still have reference to which column the row value references
+
+
+	///////////////////////////////Updating Column Stuff/////////////////////////////////////////////////
+
+	$scope.colValsToUpdate = [];
+
+	$scope.updateColumns = function(old, newColName, i){
+		$scope.columns[i] = newColName;
+
+		var colObj= {oldVal: $scope.originalColVals[i], newVal: newColName};
+
+		// if there is nothing in the array to update, push the update into it
+		if($scope.colValsToUpdate.length === 0) $scope.colValsToUpdate.push(colObj);
+				
+		// check to see if the row is already scheduled to be updated, if it is, then update it with the new thing to be updated
+		for(var e = 0; e < $scope.colValsToUpdate.length; e++){
+			if($scope.colValsToUpdate[e].old === colObj.old) $scope.colValsToUpdate = colObj;
+			else $scope.colValsToUpdate.push(colObj);
+		}
+	}
+
+	///////////////////////////////Updating Row Stuff/////////////////////////////////////////////////
+	
+	$scope.rowValsToUpdate = [];
+
+	$scope.updateRow = function(old, val, row, i){
 		row[i] = val;
 		var rowObj = {};
-		var cols = $scope.columns;
+		var cols = $scope.originalColVals;
 		for(var c = 0; c < cols.length; c++){
 			var colName = cols[c];
 			rowObj[colName] = row[c];
 		}
 
-		console.log(val)
-
 		// if there is nothing in the array to update, push the update into it
-		if($scope.valsToUpdate.length === 0) $scope.valsToUpdate.push(rowObj);
+		if($scope.rowValsToUpdate.length === 0) $scope.rowValsToUpdate.push(rowObj);
 
 		// check to see if the row is already scheduled to be updated, if it is, then update it with the new thing to be updated
-		for(var e = 0; e < $scope.valsToUpdate.length; e++){
-			if($scope.valsToUpdate[e].id === rowObj.id) $scope.valsToUpdate[e] = rowObj;
-			else $scope.valsToUpdate.push(rowObj);
+		for(var e = 0; e < $scope.rowValsToUpdate.length; e++){
+			if($scope.rowValsToUpdate[e].id === rowObj.id) $scope.rowValsToUpdate[e] = rowObj;
+			else $scope.rowValsToUpdate.push(rowObj);
 		}
-
 	}
 
 	$scope.logData=function () {
-		console.log($scope.valsToUpdate)
+		console.log($scope.rowValsToUpdate)
+		console.log($scope.colValsToUpdate)
         // if(employee !== undefined) $scope.editEmployee.push(employee);
         // console.log($scope.editEmployee);
      }
